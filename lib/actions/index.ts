644b983af903757fc8afbb7fab6f0962c68b9069
ra8200 <razi.alawamleh@gmail.com>
2023-10-19 00:03:30 -0400

    added price extraction to scraper index

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,19 @@
 'use server'
 
-import { scrapeAmazonProduct } from '../scraper';
+// import { revalidatePath } from "next/cache";
+// import Product from "../models/product.model";
+// import { connectToDB } from "../mongoose";
+import { scrapeAmazonProduct } from "../scraper";
+// import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+// import { User } from "@/types";
+// import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
  if(!productUrl) return;
  
  try {
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+    
  } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
  }
