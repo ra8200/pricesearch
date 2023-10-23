15dac3ec9e33047038a6fca4bd67f7d305d83875
ra8200 <razi.alawamleh@gmail.com>
2023-10-23 17:02:45 -0400

    emailing set up but getting errors when sending

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal"
 import PriceInfoCard from "@/components/PriceInfoCard"
 import ProductCard from "@/components/ProductCard"
 import { getProductByID, getSimilarProducts } from "@/lib/actions"
@@ -152,7 +153,7 @@ const ProductDetails = async ({ params: { id } } : Props) => {
                     </div>
                 </div>
 
-                Modal
+                <Modal productId={id} />
             </div>
         </div>
 
