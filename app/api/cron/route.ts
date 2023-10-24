247ba2c27aefb44aa4f567129a517d7370d06365
ra8200 <razi.alawamleh@gmail.com>
2023-10-24 15:55:35 -0400

    cron job finished deploying project to vercel

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,7 +1,9 @@
 import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose"
+import { generateEmailBody, sendMail } from "@/lib/nodemailer";
 import { scrapeAmazonProduct } from "@/lib/scraper";
-import { getAveragePrice, getHighestPrice, getLowestPrice } from "@/lib/utils";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
+import { NextResponse } from "next/server";
 
 export async function get() {
     try {
@@ -36,13 +38,28 @@ export async function get() {
                   product,
                 );
                
-                // 2. ChECH EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
-                const emailNotify
+                // 2. CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
+                const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct);
 
+                if(emailNotifType && updatedProduct.users.length > 0) {
+                    const productInfo = {
+                      title: updatedProduct.title,
+                      url: updatedProduct.url,
+                }
 
+                const emailContent = await generateEmailBody(productInfo, emailNotifType);
+
+                const userEmails = updatedProduct.users.map((user: any) => user.email);
+
+                await sendMail(emailContent, userEmails);
+              }
+
+              return updatedProduct;
             })
         )
-
+      return NextResponse.json({
+        message: 'Ok', data: updatedPProducts
+      })
     } catch (error) {
         throw new Error(`Error 1 Get: ${error}`)
     }
